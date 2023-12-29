338fdec5d3455ecc9f5b5787baa4d65d700527c0
ashsic <99445200+ashsic@users.noreply.github.com>
2023-12-28 19:07:22 -0500

    testing with two files

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -10,3 +10,16 @@ function component() {
   }
   
   document.body.appendChild(component());
+
+// import your function
+// import myName from './myName';
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   // use your function!
+//   element.textContent = myName('Cody');
+//   return element;
+// }
+
+// document.body.appendChild(component());
